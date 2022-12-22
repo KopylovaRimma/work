5e014aed6b7e2ac71fea8ba19c6db374df3b71a1
Aneyon <rabota1442@mail.ru>
2022-12-22 12:49:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+let a1 = 7 + 2, a2 = 7 - 3, a3 = 2 * 4, a4 = 4 / 2;
+console.log(a1, a2, a3, a4)
+
+let w = 23, h = 10, S = w * h;
+console.log(S)
+
+let b1 = 5/3, b2 = 3/5, b3 = 5 + '3', b4 = '5' - 3, b5 = 75 + 'кг';
+console.log(b1, b2, b3, b4, b5)
+
+let hC = 10, dC = 4, rC = dC / 2, v = Math.PI * rC^2 * hC;
+console.log(v)
+
+let a = 10, b = 2, cc1 = a + b, cc2 = a - b, cc3 = a * b, cc4 = a / b;
+console.log(cc1, cc2, cc3, cc4)
+
+let c = 15, d = 2, result = c + d;
+console.log(result)
+
+let aa1 = 10, bb1 = 2, cc5 = 5, dd1 = aa1 + bb1 + cc5;
+console.log(dd1)
+
+let aa2 = 17, bb2 = 10, cc6 = aa2 - bb2, dd2 = 7, result1 = aa2 + bb2 + cc6 + dd2;
+console.log(result1)
+
+let name = 'Rimma';
+console.log(`Привет ${name}`)
+
+let age = 20;
+console.log(`${age} лет!`)
+
+let str = 'abcde';
+console.log(str[0], str[2], str[4])
+
+let min = 60, hour = min * 60, day = hour * 24, mounth = day * 30;
+console.log(hour, day, mounth)
+
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+console.log(num);
+
+let num1 = 10;
+num1++;
+num1++;
+num1--;
+console.log(num1)
